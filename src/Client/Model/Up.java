63e6c7d1601fff9c1aafd9e7a9e32fbd0a4a7090
Zunaira bhatti <zunairabhatti222@gmail.com>
2023-11-28 17:39:06 +0500

    by Zunaira 3

---
+++ src/Client/Model/Up.java
@@ -0,0 +1,11 @@
+package Client.Model;
+
+import java.io.Serializable;
+
+public class Up extends Tile implements Serializable {
+    public final String imagePath="Resources/Images/up.png";
+
+    public Up(String name) {
+        super(name);
+    }
+}
